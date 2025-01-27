b5b8b1d1f2b760032968a2e8e2135f1db780095f
imdavyking <yungdynamic53@gmail.com>
2025-01-27 13:49:45 +0100

    updated

--- src/validations/user.ts
+++ src/validations/user.ts
@@ -4,3 +4,12 @@ export const createTweetsSchema = Joi.object({
   id: Joi.string().required(),
   edit_history_tweet_ids: Joi.array<string>().required(),
 });
+
+export const validateTweet = (req, res, next) => {
+  const { error } = createTweetsSchema.validate(req.body);
+  if (error) {
+    error.isJoi = true; // Mark this as a Joi validation error
+    return next(error);
+  }
+  next();
+};
