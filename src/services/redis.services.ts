15f5a082a55505e301fb89dd89a963cdc9c6d61c
imdavyking <yungdynamic53@gmail.com>
2025-01-27 16:28:09 +0100

    chore: removed duplicating number in redis

--- src/services/redis.services.ts
+++ src/services/redis.services.ts
@@ -5,7 +5,6 @@ const redis = new Redis({
   host: process.env.REDIS_HOST! || "localhost",
   port: +process.env.REDIS_PORT! || 6379,
   password: process.env.REDIS_PASSWORD,
-  db: 0,
 });
 
 redis.on("connect", () => {
